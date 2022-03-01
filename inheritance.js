f3b0a96551a6bc3fd4b0b7847a65a21335ed8413
unknown <you@example.com>
2022-03-01 10:41:05 +0500

    continue

--- inheritance.js
+++ inheritance.js
@@ -25,3 +25,33 @@ console.log(jhon.__proto__==Person.prototype)
 Person.prototype.country='Pakistan'
 
 console.log(jhon.country)
+
+//prototypal inheritance
+
+function Clerk(design){
+    this.design=design
+}
+
+Clerk.prototype=jhon
+
+const clrk=new Clerk('Clerk')
+console.log(clrk)
+
+
+const P1={
+
+    name:"Mudasar",
+    fullName:function(){
+        console.log(`My name is ${this.name}`)
+    }
+}
+
+const P2={
+    city:"KBW",
+    name:"Ali",
+    __proto__:P1
+}
+
+
+console.log(P2.name)
+P2.fullName()
